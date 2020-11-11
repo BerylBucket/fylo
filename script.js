55388dd9862f6f0780d534dce597ecd64ce03f7f
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-11-11 12:30:35 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("gb");
+animateValue(obj, 0, 185, 1500);
